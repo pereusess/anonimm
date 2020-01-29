2bfb7800fa54cbfa105bb946875981812eed879e
andrei <andrei.potupa91@mail.ru>
2020-01-29 21:20:15 +0800

    added

--- homework2.py
+++ homework2.py
@@ -1,4 +1,54 @@
-array = [1,"name",True,None]
+#HOMEWORK NO: 1
+# array = [1,"string",True,None,{"name":"andy"}]
+#
+# for item in array:
+#    print(type(item))
 
-for item in array:
-	print(type(item))
+#HOMEWORK NO:2
+#i didnt understand the question. my russian is not perfect.
+
+
+#HOMEWORK NO:3
+# months = {
+#     "winter" : [12,1,2],
+#     "spring" : [3,4,5],
+#     "summer" : [6,7,8],
+#     "autumn" : [9,10,11]
+# }
+# month = int(input("month: "))
+# for i in months:
+#     if month in months[i]:
+#         print(i)
+
+#HOMEWORK NO:4
+#I couldnt find the solution by using input and make new line.
+# string = "house","car","kalashnikov","villa"
+# words = []
+# n = 0
+# for i in string:
+#     new = i[:10:]
+#     words.append((n,new))
+#     n += 1
+# print(words)
+
+#HOMEWORK NO:5
+# nums = [7,3,4,2]
+# count = 0
+# while count < 10:
+#     count += 1
+#     num = int(input("numbers:"))
+#     nums.append(int(num))
+#     nums.sort()
+#     print(nums)
+
+#HOMEWORK NO:6
+# products =[
+# (1,{"name":"laptop","price":1000,"quantity":5}),
+# (2,{"name":"mouse","price":500,"quantity":10}),
+# (3,{"name":"keypad","price":700,"quantity":15})
+# ]
+# 
+# for i in products:
+#     new = i[1]
+#     for j,k in new.items():
+#             print(j,k)
